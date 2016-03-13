c12129e658ce80de0e8367ab515b9331c5670513
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-13 15:45:05 +0100

    Also test to build PDF

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_gcov
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_boost_gcov
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_boost_gcov_bigger_project
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_gcov
 #git clone https://github.com/richelbilderbeek/qmake_gcov_bigger_project
 #git clone https://github.com/richelbilderbeek/qmake_gprof
+lyx --export pdf2 travis_cpp_tutorial.lyx
