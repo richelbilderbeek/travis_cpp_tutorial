aaa6e6f6c9bf23641e0699e80858de506c3d69e0
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-13 16:14:00 +0100

    Convert to older version manually, convert to PDF automatically

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_gcov
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_gcov
 #git clone https://github.com/richelbilderbeek/qmake_gcov_bigger_project
 #git clone https://github.com/richelbilderbeek/qmake_gprof
-lyx --export pdf2 travis_cpp_tutorial.lyx
+lyx --export pdf2 travis_cpp_tutorial.20.lyx
