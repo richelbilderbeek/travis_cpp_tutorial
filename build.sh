8b497b7af5108065f4cfa3b3992f26c81ef006d7
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-13 20:54:35 +0100

    C++11 added

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_boost
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_gcov
+git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_boost_gcov
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_boost_gcov_bigger_project
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_gcov
