c2a939b53f142575ab309e0f0d65460d55cc17e0
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-14 16:09:25 +0100

    PDF is checked to be built correctly

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_boost
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_gcov
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11
+git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_boost_test
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp14
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_boost_gcov
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_boost_gcov_bigger_project
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_gcov
 #git clone https://github.com/richelbilderbeek/qmake_gcov_bigger_project
 #git clone https://github.com/richelbilderbeek/qmake_gprof
-#lyx --export pdf2 travis_cpp_tutorial.20.lyx
 lyx --export pdf2 travis_cpp_tutorial.lyx
