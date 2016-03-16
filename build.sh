33ec0738f62b6f8785f06bd5bc40f36be5992b9e
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-16 15:38:38 +0100

    Force overwrite for Travis

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp14_boost
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_gcov
 #git clone https://github.com/richelbilderbeek/qmake_gcov_bigger_project
 #git clone https://github.com/richelbilderbeek/qmake_gprof
-lyx --export pdf2 travis_cpp_tutorial.lyx
+lyx -f --export pdf2 travis_cpp_tutorial.lyx
