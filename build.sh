5f5a4b01a37f5190527b086dce8e4f44b96cafa1
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-16 21:39:30 +0100

    Travis: works again?

--- build.sh
+++ build.sh
@@ -3,9 +3,12 @@ git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_boost
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_gcov
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_rcpp
+git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_sfml
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_boost
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_boost_test
+#git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_sfml
+git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_rcpp
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp14
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp14_boost
 #git clone https://github.com/richelbilderbeek/cpp11_qmake_boost_gcov
