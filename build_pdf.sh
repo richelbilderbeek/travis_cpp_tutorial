ca93a59ad765d24f0b895b949cf55e27b2617cab
richelbilderbeek <richel@richelbilderbeek.nl>
2016-04-17 00:12:57 +0200

    Use pdflatex

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-lyx -f --export pdf travis_cpp_tutorial.lyx
+#lyx -f --export pdf travis_cpp_tutorial.lyx
+lyx -f --export pdflatex travis_cpp_tutorial.lyx
+pdflatex travis_cpp_tutorial
+pdflatex travis_cpp_tutorial
