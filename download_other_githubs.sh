adaa56af83650ed36266ac4b9b92fee3b5681e29
richelbilderbeek <richel@richelbilderbeek.nl>
2016-07-30 15:42:58 +0200

    Added OCLinter

--- download_other_githubs.sh
+++ download_other_githubs.sh
@@ -8,6 +8,7 @@ git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_boost_test
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_boost_test_gcov
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_debug_and_release
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_gcov
+git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_oclint
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_qt
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_qt_qtest
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_rcpp
