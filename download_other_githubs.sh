7ee02284cb13d090be69c229f1bbf4c9964fa495
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-12 14:54:38 +0200

    EOD

--- download_other_githubs.sh
+++ download_other_githubs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd ..
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_debug_gcov
 git clone https://github.com/richelbilderbeek/travis_qmake_clang_cpp98
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98
@@ -17,7 +16,6 @@ git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_qt5
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_qt_qtest
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_rcpp
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_sfml
-git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_sloccount
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_urho3d
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp98_wt
 git clone https://github.com/richelbilderbeek/travis_qmake_clang_cpp11
@@ -29,6 +27,7 @@ git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_boost_test_
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_gcov
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_qt
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_qt_qtest
+git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_sloccount
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_sfml
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_rcpp
 git clone https://github.com/richelbilderbeek/travis_qmake_gcc_cpp11_urho3d
